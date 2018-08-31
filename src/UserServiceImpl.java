cd77ad7f2b7a988ede345b1a4e34e6b7618abcef
dpk555 <sahudeepak154@gmail.com>
2018-08-31 18:30:33 +0530

    added new proj

---
+++ src/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.vaannila.service;
+
+import com.vaannila.domain.User;
+
+public class UserServiceImpl implements UserService {
+	@Override
+	public void add(User user) {
+		//Persist the user object here. 
+
+	}
+
+}
